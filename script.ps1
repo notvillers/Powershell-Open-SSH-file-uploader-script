5b22d426971c3510526042b9bb3544e29482aaf8
Villers <117743062+notvillers@users.noreply.github.com>
2023-11-20 22:46:15 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -12,27 +12,38 @@ param (
     [string]$destination
 )
 
-# Checks the Posh-SSH module, if not found then installs it
-if (-not (Get-Module Posh-SSH -ListAvailable)) {
-    Write-Host "Posh-SSH not found, installing NuGet package provider and Posh-SSH"
-    Install-PackageProvider NuGet -Force
-    Install-Module Posh-SSH -Confirm:$False -Force
+try {
+    # Checks the Posh-SSH module, if not found then installs it
+    if (-not (Get-Module Posh-SSH -ListAvailable)) {
+        Write-Host "Posh-SSH not found, installing NuGet package provider and Posh-SSH module"
+        Install-PackageProvider NuGet -Force
+        Install-Module Posh-SSH -Confirm:$False -Force
+    }
+
+    if (Test-Connection -TargetName $hostname -IPv4 -Count 1 -Quiet) {
+
+        # Imports the module
+        Import-Module Posh-SSH
+
+        # Converting [string] to [securestring]
+        $passwordSec = $password | ConvertTo-SecureString -AsPlainText -Force
+
+        # Checking the $FilePath
+        if (Test-Path -path $path) {
+            # Creates a credential
+            $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $login,$passwordSec
+            # Creates a session
+            $session = New-SFTPSession -ComputerName $hostname -Credential $credential -AcceptKey
+            # Copies the source item to the destination
+            Set-SFTPItem -Session $session.SessionID -Destination $destination -Path $path -verbose -Force
+        } else {
+            Write-Host "$path not found"
+        }
+
+    } else {
+        Write-Host "$hostname not available"
+    }
 }
-
-# Imports the module
-Import-Module Posh-SSH
-
-# Converting [string] to [securestring]
-$passwordSec = $password | ConvertTo-SecureString -AsPlainText -Force
-
-# Checking the $FilePath
-if (Test-Path -path $path) {
-    # Creates a credential
-    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $login,$passwordSec
-    # Creates a session
-    $session = New-SFTPSession -ComputerName $hostname -Credential $credential -AcceptKey
-    # Copies the source item to the destination
-    Set-SFTPItem -Session $session.SessionID -Destination $destination -Path $path -verbose -Force
-} else {
-    Write-Host "$path not found."
+catch {
+    Write-Host "An error occurred that could not be resolved"
 }
