fcdcd7364aadcd323f697724137c9737d5a0f860
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-11-23 14:11:15 +0100

    updt

--- script.ps1
+++ script.ps1
@@ -9,41 +9,67 @@ param (
     [Parameter()]
     [string]$path,
     [Parameter()]
-    [string]$destination
+    [string]$destination,
+    [Parameter()]
+    [string]$log
 )
 
 try {
     # Checks the Posh-SSH module, if not found then installs it
     if (-not (Get-Module Posh-SSH -ListAvailable)) {
-        Write-Host "Posh-SSH not found, installing NuGet package provider and Posh-SSH module"
+        $currentDateTime = Get-Date
+        $currentDateTimeString = $currentDateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        Add-Content -Path $logPath -Value "$currentDateTimeString : Posh-SSH not found, installing NuGet package provider and Posh-SSH module"
         Install-PackageProvider NuGet -Force
         Install-Module Posh-SSH -Confirm:$False -Force
     }
 
-    if (Test-Connection -TargetName $hostname -IPv4 -Count 1 -Quiet) {
+    # If -log is not empty then uses it as the $logName
+    $scriptDirectory = $PSScriptRoot
+    if ($log -eq "") {
+        $logName = "log.txt"
+    }
+    else {
+        $logName = $log
+    }
+    $logPath = Join-Path -Path $scriptDirectory -ChildPath $fileName
 
-        # Imports the module
-        Import-Module Posh-SSH
-
-        # Converting [string] to [securestring]
-        $passwordSec = $password | ConvertTo-SecureString -AsPlainText -Force
-
-        # Checking the $FilePath
-        if (Test-Path -path $path) {
-            # Creates a credential
-            $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $login,$passwordSec
-            # Creates a session
-            $session = New-SFTPSession -ComputerName $hostname -Credential $credential -AcceptKey
-            # Copies the source item to the destination
-            Set-SFTPItem -Session $session.SessionID -Destination $destination -Path $path -verbose -Force
-        } else {
-            Write-Host "$path not found"
-        }
-
+    # Creates $logPath file, if not found
+    if (Test-Path -path $logPath) {
+        $currentDateTime = Get-Date
+        $currentDateTimeString = $currentDateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        Add-Content -Path $logPath -Value "$currentDateTimeString : $logName found"
     } else {
-        Write-Host "$hostname not available"
+        $currentDateTime = Get-Date
+        $currentDateTimeString = $currentDateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        Out-File -FilePath $filePath -InputObject "$currentDateTimeString : $fileName created"
+    }
+
+    # Imports the module
+    Import-Module Posh-SSH
+
+    # Converting [string] to [securestring]
+    $passwordSec = $password | ConvertTo-SecureString -AsPlainText -Force
+
+    # Checking the $FilePath
+    if (Test-Path -path $path) {
+        # Creates a credential
+        $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $login,$passwordSec
+        # Creates a session
+        $session = New-SFTPSession -ComputerName $hostname -Credential $credential -AcceptKey
+        # Copies the source item to the destination
+        Set-SFTPItem -Session $session.SessionID -Destination $destination -Path $path -verbose -Force
+        $currentDateTime = Get-Date
+        $currentDateTimeString = $currentDateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        Add-Content -Path $logPath -Value "$currentDateTimeString : $path put in $destination"
+    } else {
+        $currentDateTime = Get-Date
+        $currentDateTimeString = $currentDateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        Add-Content -Path $logPath -Value "$currentDateTimeString : $path not found"
     }
 }
 catch {
-    Write-Host "An error occurred that could not be resolved"
+    $currentDateTime = Get-Date
+    $currentDateTimeString = $currentDateTime.ToString("yyyy-MM-dd HH:mm:ss")
+    Add-Content -Path $logPath -Value "$currentDateTimeString : An error occurred that could not be resolved"
 }
